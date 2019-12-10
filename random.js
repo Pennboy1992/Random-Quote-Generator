0978b7af16575a4bb1d960cc9c45946fd71d96e4
Mad Marine <pnnroman92@gmail.com>
2019-12-09 20:52:39 -0600

    code cleanup

--- random.js
+++ random.js
@@ -1,28 +1,22 @@
 'use strict';
 
-
-
 var quoteField = document.getElementById('quoteField');
 var author = document.getElementById('authorField');
 var btn = document.getElementById('btn');
-
 
 var quotes = [
 { 
   word: "But that was life: Nobody got a guided tour to their own theme park. You had to hop on the rides as they presented themselves, never knowing whether you would like the one you were in line for...or if the bastard was going to make you throw up your corn dog and your cotton candy all over the place.",
   author: "-J.R. Ward"
 },
-
 { 
   word: "I have no special talent. I am only passionately curious.",
   author: "-Albert Einstein"
 },
-
 { 
   word: "If you judge people, you have no time to love them.",
   author: "-Mother Teresa"
 },
-
 {
   word: "Stay hungry, stay foolish.",
   author: "-Steve Jobs"
